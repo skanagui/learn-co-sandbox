e3d4ba5bdaa9afe2ccd84fe7086122e416780160
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-23 23:43:28 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,10 @@
+puts "Hello World"
+
+greeting = "friendly_greeting"
+ 
+case greeting
+  when "unfriendly_greeting"
+    puts "What do you want!?"
+  when "friendly_greeting"
+    puts "Hi! How are you?"
+end
---
+++ greeting.rb
@@ -0,0 +1,14 @@
+def greeting
+  puts "Hello World"
+end
+
+def say_greeting_x_times
+greeting 
+greeting 
+greeting
+greeting
+greeting
+greeting
+end
+
+say_greeting_x_times
